9a79e8c1115826853d60daa98635fb17fb0516cd
JOSU9435 <kamalnetra702@gmail.com>
2022-05-21 22:18:30 +0530

    ADD: audio streaming

--- src/index.js
+++ src/index.js
@@ -1,6 +1,7 @@
 import dotenv from "dotenv";
 import { Client, Intents } from "discord.js";
 import join from "./utils/join.js";
+import play from "./utils/play.js";
 
 
 dotenv.config({path: "./src/.env"});
@@ -8,6 +9,7 @@ dotenv.config({path: "./src/.env"});
 const client = new Client({ intents: [
     "GUILDS",
     "GUILD_MESSAGES",
+    "GUILD_VOICE_STATES",
 ] })
 
 const prefix = "-";
@@ -17,13 +19,13 @@ let connection = null;
 client.on("messageCreate", async (msg) => {
 
     const [command, args] = msg.content.indexOf(" ")===-1 ? [msg.content,""] : [msg.content.substring(0,msg.content.indexOf(" ")), msg.content.substring(msg.content.indexOf(" ")+1)];
-    
+
     if(command===`${prefix}play`){
-        if(connection!==null){
-            msg.reply("already in a voice channel disconnect first");
-            return;
+        if(connection===null){
+            connection = join(msg);        
         }
-        connection = join(msg);        
+
+        play(args,connection);
     }
     else if(command===`${prefix}disconnect`){
         connection?.destroy();
--- src/utils/play.js
+++ src/utils/play.js
@@ -1,6 +1,30 @@
 import Discord from "discord.js";
+import { AudioPlayerStatus, createAudioPlayer, createAudioResource } from "@discordjs/voice";
+import ytdl from "ytdl-core";
 
-export const play = () => {
+const play = async (url, connection) => {
+    // console.log(url);
 
+    const stream = await ytdl(url,{filter: "audioonly",})
+    // console.log(stream)
+
+    const player = createAudioPlayer();
+
+    const audioResource = await createAudioResource(stream,{ inlineVolume: 1});
+
+    // const audioResource = createAudioResource("/home/josu/Downloads/test.mpeg", {
+    //     inlineVolume: 1,
+    // });
     
+    player.play(audioResource);
+    
+    connection.subscribe(player);
+
+    // console.log(audioResource);
+
+    player.on(AudioPlayerStatus.Idle, () => {
+        connection.destroy();
+    })
 }
+
+export default play;
