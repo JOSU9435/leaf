e833b9efc558398e0958c485ae6aa9f9a8061ffc
JOSU9435 <kamalnetra702@gmail.com>
2022-07-20 15:58:55 +0530

    ADD: check if the vc is joinable

--- src/utils/join.js
+++ src/utils/join.js
@@ -4,7 +4,7 @@ import AudioState from "../models/AudioState.js";
 import disconnect from "../commandHandlers/disconnect.js";
 
 /**
- * @description initialize the connection to the voice channel, the audio player and the songQueue returns a audioState with the player instance and the connection instance and the songQueue.
+ * @description initialize the connection to the voice channel, the audio player and the songQueue returns a audioState with the player instance and the connection instance and the songQueue and returns an empty object if any err is occured.
  * @param {Message} msg 
  * @returns {AudioState}
  */
@@ -14,11 +14,21 @@ const handleJoin = (msg) => {
     try {
         
         const memberVoiceChannel = msg?.member?.voice?.channel;
+
         if(!memberVoiceChannel){
             const enterVcMessage = new MessageEmbed();
             enterVcMessage.setTitle("You need to be in a VC first").setColor("WHITE");
     
             msg.reply({embeds: [enterVcMessage]});
+            return {};
+        }
+
+        if(!memberVoiceChannel?.joinable){
+            const unableJoinMessage = new MessageEmbed();
+        
+            unableJoinMessage.setTitle("Unable to join the VC").setColor("WHITE");
+            msg?.channel?.send({embeds: [unableJoinMessage]});
+
             return {};
         }
     
